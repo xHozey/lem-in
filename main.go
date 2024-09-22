286202c6d33939b9e6e247da61ae7f846f522cf4
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-09-22 11:18:38 +0100

    inhance main

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 )
 
 func main() {
+	// Check args
 	args := os.Args[1:]
 	if len(args) != 1 || !utils.CheckFileName(args[0]) {
 		fmt.Fprintln(os.Stderr, "Usage: go run . filename.txt")
 		return
 	}
 
+	// Read File And parse data
 	fileData, err := utils.ReadFile(args[0])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Please Write a valid file")
@@ -26,6 +28,7 @@ func main() {
 		return
 	}
 
+	// Intialize Graph
 	graph := utils.Graph{}
 
 	for _, room := range parsedData.Rooms {
@@ -36,6 +39,7 @@ func main() {
 		graph.AddEdge(links[0], links[1])
 	}
 
+	// Get All possible paths
 	paths := [][]string{}
 	startVertex := utils.GetVertex(graph.Vertecies, parsedData.Start)
 	targetVertex := utils.GetVertex(graph.Vertecies, parsedData.End)
@@ -43,8 +47,10 @@ func main() {
 	graph.CDFS(startVertex, targetVertex, []string{}, &paths)
 	utils.Sort(&paths)
 
+	// Sepearate Paths in unique combinations
 	disjointPaths := utils.SepRoads(&paths)
 
+	// Get All Possible Results and print the most optimal
 	results := []string{}
 
 	for _, combPaths := range disjointPaths {
@@ -57,5 +63,5 @@ func main() {
 		results = append(results, strRes)
 	}
 
-	fmt.Println(utils.GetMinPath(results))
+	fmt.Print(utils.GetMinPath(results))
 }
